eb907eec3f8a034a7889fd1c2b2b06e54cbfeb2f
domfarr98 <domfarr98@gmail.com>
2019-12-28 18:36:03 +0000

    -Ordered the definitions of the Entry UI's assets. -Added a boiler-plate to prevent un-wanted running. -Added a begin_button_press function when the begin button is pressed (this passes the entry field's values).

--- src/main.py
+++ src/main.py
@@ -1,40 +1,39 @@
-# import tkinter module
 from tkinter import *
 from tkinter.ttk import *
 
-# creating main tkinter window/toplevel
-master = Tk()
 
-# this wil create a label widget
-l0 = Label(master, text="The Game of Life")
-l1 = Label(master, text="Rows:")
-l2 = Label(master, text="Columns:")
-l3 = Label(master, text="Seed:")
-l4 = Button(text="Start", width=25)
+def begin_button_press(xValue, yValue, seedValue):
 
-l0.config(font=44)
+    # placeholder!
+    print(xValue, " ", yValue, " ", seedValue)
 
-# grid method to arrange labels in respective
-# rows and columns as specified
-l0.grid(row=0, column=0, columnspan=2, pady=2)
-l1.grid(row=1, column=0, sticky=W, pady=2)
-l2.grid(row=2, column=0, sticky=W, pady=2)
-l3.grid(row=3, column=0, sticky=W, pady=2)
-l4.grid(row=4, column=0, columnspan=2, pady=2)
 
-master.grid_rowconfigure(0, minsize=30)
-master.grid_rowconfigure(4, minsize=50)
+if __name__ == "__main__":
+    screen = Tk()
+    screen.grid_rowconfigure(0, minsize=30)
+    screen.grid_rowconfigure(4, minsize=50)
 
-# entry widgets, used to take entry from user
-e1 = Entry(master)
-e2 = Entry(master)
-e3 = Entry(master)
+    title = Label(screen, text="The Game of Life", font=44)
+    title.grid(row=0, column=0, columnspan=2, pady=2)
 
-# this will arrange entry widgets
-e1.grid(row=1, column=1, pady=2)
-e2.grid(row=2, column=1, pady=2)
-e3.grid(row=3, column=1, pady=2)
+    xLabel = Label(screen, text="Rows:")
+    xLabel.grid(row=1, column=0, sticky=W, pady=2)
 
-# infinite loop which can be terminated by keyboard
-# or mouse interrupt
-mainloop()
+    yLabel = Label(screen, text="Columns:")
+    yLabel.grid(row=2, column=0, sticky=W, pady=2)
+
+    seedLabel = Label(screen, text="Seed:")
+    seedLabel.grid(row=3, column=0, sticky=W, pady=2)
+
+    xEntry = Entry(screen)
+    yEntry = Entry(screen)
+    seedEntry = Entry(screen)
+
+    xEntry.grid(row=1, column=1, pady=2)
+    yEntry.grid(row=2, column=1, pady=2)
+    seedEntry.grid(row=3, column=1, pady=2)
+
+    beginButton = Button(text="Start", width=25, command=lambda: begin_button_press(xEntry.get(), yEntry.get(), seedEntry.get()))
+    beginButton.grid(row=4, column=0, columnspan=2, pady=2)
+
+    mainloop()
