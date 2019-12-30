cf58adffdfb82f1fc7ec553184e3c557a31db8e6
domfarr98 <domfarr98@gmail.com>
2019-12-30 12:45:53 +0000

    -Created movement calculation tests based on the specifications given in the work sheet.

--- test/program _test.py
+++ test/program _test.py
@@ -1,19 +1,83 @@
 import unittest
 from src import program
 
+
 # this set of tests will aim to test the programs methods, such as its ability to move/evolve cells depending on their
 # layout, as well as its ability to generate seeded playing fields.
 class TestProgram(unittest.TestCase):
 
-    def test_test(self):
-        self.assertEqual(program.calculate_moves([]), True)
+    def test_scenario_0_no_interactions(self):
+        input_array = [[False, False, False],
+                       [False, False, False],
+                       [False, False, False]]
 
-    def test_scenario_1(self):
-        self.assertEqual(program.calculate_moves([]), True)
+        output_array = [[False, False, False],
+                        [False, False, False],
+                        [False, False, False]]
+
+        self.assertEqual(program.calculate_moves(input_array), output_array)
+
+    def test_scenario_1_underpopulation(self):
+        input_array = [[False, False, False],
+                       [False, True, False],
+                       [False, True, False]]
+
+        output_array = [[False, False, False],
+                        [False, False, False],
+                        [False, False, False]]
+
+        self.assertEqual(program.calculate_moves(input_array), output_array)
+
+    def test_scenario_2_overcrowding(self):
+        input_array = [[False, True, True],
+                       [False, True, True],
+                       [False, True, True]]
+
+        output_array = [[False, True, True],
+                        [False, False, False],
+                        [False, True, True]]
+
+        self.assertEqual(program.calculate_moves(input_array), output_array)
+
+    def test_scenario_3_survival(self):
+        input_array = [[False, False, False],
+                       [False, True, True],
+                       [False, True, True]]
+
+        output_array = [[False, False, False],
+                        [False, True, True],
+                        [False, True, True]]
+
+        self.assertEqual(program.calculate_moves(input_array), output_array)
+
+    def test_scenario_4_creation_of_life(self):
+        input_array = [[False, False, False],
+                       [False, False, True],
+                       [False, True, True]]
+
+        output_array = [[False, False, False],
+                        [False, True, True],
+                        [False, True, True]]
+
+        self.assertEqual(program.calculate_moves(input_array), output_array)
+
+    def test_scenario_6_expected_game_outcome_for_seeded_grid(self):
+        input_array = [[False, True, False],
+                       [False, True, False],
+                       [False, True, False]]
+
+        output_array = [[False, False, False],
+                        [True, True, True],
+                        [False, False, False]]
+
+        self.assertEqual(program.calculate_moves(input_array), output_array)
+        self.assertEqual(program.calculate_moves(output_array), input_array)
 
     def test_seeded_generation(self):
-        test_array = [[False, False, False, False, False], [True, False, False, False, False],
-                      [True, True, False, True, True], [False, True, False, False, True],
+        test_array = [[False, False, False, False, False],
+                      [True, False, False, False, False],
+                      [True, True, False, True, True],
+                      [False, True, False, False, True],
                       [True, True, False, True, False]]
         self.assertEqual(program.create_seeded_playing_field(5, 5, "hjejs"), test_array)
 
